67d9957e6b4fd3f34fa9ce5c85b0fff46f0a6f47
balajisa <sabalaji09@gmail.com>
2024-01-19 12:20:34 +0000

    add tls cert restore aws keys

--- main.tf
+++ main.tf
@@ -61,6 +61,14 @@ variable "tls_cert_backup_s3_key_prefix" {
   description = "S3 bucket backup folder prefix"
 }
 
+variable "tls_cert_restore_aws_access_key" {
+  description = "AWS access key for cert manager"
+}
+
+variable "tls_cert_restore_aws_secret_key" {
+  description = "AWS secret key for cert manager"
+}
+
 variable "loki_aws_access_key" {
   description = "AWS access key for Loki"
 }
@@ -221,7 +229,7 @@ variable "glueops_operators_web_acl_aws_secret_key" {
 
 output "helm_values" {
   value = replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(
-    replace(replace(replace(replace(
+    replace(replace(replace(replace(replace(replace(
       data.local_file.platform_values_template.content,
     "placeholder_tenant_key", var.tenant_key),
     "placeholder_captain_repo_b64enc_private_deploy_key", var.captain_repo_b64encoded_private_deploy_key),
@@ -242,6 +250,8 @@ output "helm_values" {
     "placeholder_certmanager_aws_secret_key", var.certmanager_aws_secret_key),
     "placeholder_tls_cert_backup_aws_access_key", var.tls_cert_backup_aws_access_key),
     "placeholder_tls_cert_backup_aws_secret_key", var.tls_cert_backup_aws_secret_key),
+    "placeholder_tls_cert_restore_aws_access_key", var.tls_cert_restore_aws_access_key),
+    "placeholder_tls_cert_restore_aws_secret_key", var.tls_cert_restore_aws_secret_key),
     "placeholder_tls_cert_backup_s3_key_prefix", var.tls_cert_backup_s3_key_prefix),
     "placeholder_loki_aws_access_key", var.loki_aws_access_key),
     "placeholder_loki_aws_secret_key", var.loki_aws_secret_key),
