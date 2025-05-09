2ac9e2fc3271fe3a000d2cb17cc6c565aeb521cf
Bazmahou <hamzabouissi@sousse.r-iset.tn>
2025-05-09 16:38:38 +0100

    refactor

--- main.tf
+++ main.tf
@@ -215,7 +215,7 @@ variable "tenant_s3_multi_region_access_point" {
 
 
 output "helm_values" {
-  value = replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(
+  value = replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(
     replace(
       data.local_file.platform_values_template.content,
     "placeholder_tenant_key", var.tenant_key),
